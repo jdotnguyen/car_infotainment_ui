faf16ed3c135b9928553111e86b5d46153500d47
Justin Nguyen <justin@gfisystems.ca>
2017-11-02 20:55:34 -0400

    Entertainment section

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,18 @@
 /* Document Ready */
 $(document).ready(function() {
+	/* Show home page */
+	$('.home-page').fadeIn();
+
 	/* Add click handlers */
-	addClickHandlers();
+	addHomePageClickHandlers();
+	addEntertainmentPageClickHandlers();
+	addCommunicationPageClickHandlers();
+	addNotificationPageClickHandlers();
+	addSettingsPageClickHandlers();
 });
 
-/* Click handlers */
-addClickHandlers = function () {
+/* Home page handlers */
+addHomePageClickHandlers = function () {
 	/* Home page card click handlers */
 	$('.card-ui').click(function () {
 		var id = $(this)[0].id;
@@ -43,4 +50,96 @@ addClickHandlers = function () {
 		/* Show home page */
 		$('.home-page').fadeIn();
 	});
+};
+
+/* Entertainment page handlers */
+addEntertainmentPageClickHandlers = function () {
+	/* Source button handlers */
+	$('.source-btn-body').click(function () {
+		var id = $(this)[0].id;
+
+		/* Clear active state from all buttons */
+		$('.source-btn-body').removeClass('active');
+
+		/* Style each section accordingly */
+		switch(id) {
+			case "source-btn-radio":
+			case "source-btn-satellite":
+				/* Add active class to button */
+				$('#' + id).addClass('active');
+
+				/* Show/hide radio header */
+				$('.song-info-span-header-radio').fadeIn();
+
+				/* Show/hide play controls */
+				$('.play-controls-row').hide();
+
+				/* Reset play controls buttons */
+				$('.play-controls-btn').removeClass('active');
+				$('#play-controls-play').addClass('active');
+				break;
+			case "source-btn-usb":
+			case "source-btn-bluetooth":
+				/* Add active class to button */
+				$('#' + id).addClass('active');
+
+				/* Show/hide radio header */
+				$('.song-info-span-header-radio').hide();
+
+				/* Show/hide play controls */
+				$('.play-controls-row').fadeIn();
+				break;
+		}
+	});
+
+	/* Play control button handlers */
+	$('.play-controls-btn').click(function () {
+		var id = $(this)[0].id;
+
+		/* Clear active state from all buttons */
+		$('.play-controls-btn').removeClass('active');
+
+		/* Style each section accordingly */
+		switch(id) {
+			case "play-controls-previous":
+				/* Add active class to button */
+				$('#' + id).addClass('active');
+				break;
+			case "play-controls-backward":
+				/* Add active class to button */
+				$('#' + id).addClass('active');
+				break;
+			case "play-controls-play":
+				/* Add active class to button */
+				$('#' + id).addClass('active');
+				break;
+			case "play-controls-forward":
+				/* Add active class to button */
+				$('#' + id).addClass('active');
+				break;
+			case "play-controls-next":
+				/* Add active class to button */
+				$('#' + id).addClass('active');
+				break;
+			case "play-controls-stop":
+				/* Add active class to button */
+				$('#' + id).addClass('active');
+				break;
+		}
+	});
+};
+
+/* Communication page handlers */
+addCommunicationPageClickHandlers = function () {
+
+};
+
+/* Notification page handlers */
+addNotificationPageClickHandlers = function () {
+
+};
+
+/* Settings page handlers */
+addSettingsPageClickHandlers = function () {
+
 };
